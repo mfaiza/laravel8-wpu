01a04a1a07ad7bc2e7c8e856ce0ddfed3ed3113b
mfaiza <mochfaizalamsyah@gmail.com>
2023-05-19 16:54:34 +0700

    add post model and layout

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -3,6 +3,6 @@
     Ini Halaman About
     <p>{{ $name }}</p>
     <p>{{ $no_telp }}</p>
-    <img src="img/kafka.jpg" alt="{{ $name }}" width="200">
+    <img src="img/kafka.jpg" alt="{{ $name }}" width="200" class="img-thumbnail rounded-circle">
     <script src="js/script.js"></script>
 @endsection
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Models\Post;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -27,52 +27,15 @@ Route::get('/about', function () {
 });
 
 Route::get('/posts', function () {
-    $blog_posts = [
-        [
-            'title' => "Post pertama",
-            'slug' => 'post-pertama',
-            'author' => 'Faiz Alam',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In temporibus excepturi fugiat esse voluptas ipsa quidem pariatur, officia neque harum. Sequi odio cumque alias! Enim eligendi rerum, optio hic magnam molestiae atque quasi consectetur alias aspernatur asperiores! Nihil enim omnis ducimus libero modi porro quam voluptas aperiam alias dicta! Minima, voluptatum cum aliquid optio nesciunt eius, est atque, eveniet nam veritatis laboriosam excepturi necessitatibus rem cupiditate enim neque? Quam modi repellat neque rem illum quaerat eos totam corporis molestias eius!'
-        ],
-        [
-            'title' => "Post Kedua",
-            'slug' => "post-kedua",
-            'author' => 'Cusson Baby',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi excepturi facere natus quod eos sunt dolorum possimus asperiores officiis voluptatibus, corrupti deleniti obcaecati vel nobis voluptatem amet? Harum dicta quibusdam amet totam, placeat mollitia labore consequatur quam qui. Et voluptatum impedit blanditiis corporis libero dolorum? Esse distinctio facere tenetur, possimus veniam vel aut tempore animi laborum. Ullam veritatis tempora, doloremque consequatur dolorem exercitationem numquam eos illum fugiat repudiandae facere at unde cum modi iusto dicta rerum officia culpa quod sed temporibus quia. Consequuntur quibusdam id ipsam pariatur, dolorum, minima exercitationem enim eum tempora, quam eius libero? Eum harum vero libero!'
-        ]
-    ];
-
     return view('posts', [
         "title" => "Posts",
-        "posts" => $blog_posts,
+        "posts" => Post::all(),
     ]);
 });
 
 Route::get('/posts/{slug}', function ($slug) {
-    $blog_posts = [
-        [
-            'title' => "Post pertama",
-            'slug' => 'post-pertama',
-            'author' => 'Faiz Alam',
-            'body' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. In temporibus excepturi fugiat esse voluptas ipsa quidem pariatur, officia neque harum. Sequi odio cumque alias! Enim eligendi rerum, optio hic magnam molestiae atque quasi consectetur alias aspernatur asperiores! Nihil enim omnis ducimus libero modi porro quam voluptas aperiam alias dicta! Minima, voluptatum cum aliquid optio nesciunt eius, est atque, eveniet nam veritatis laboriosam excepturi necessitatibus rem cupiditate enim neque? Quam modi repellat neque rem illum quaerat eos totam corporis molestias eius!'
-        ],
-        [
-            'title' => "Post Kedua",
-            'slug' => "post-kedua",
-            'author' => 'Cusson Baby',
-            'body' => 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Sequi excepturi facere natus quod eos sunt dolorum possimus asperiores officiis voluptatibus, corrupti deleniti obcaecati vel nobis voluptatem amet? Harum dicta quibusdam amet totam, placeat mollitia labore consequatur quam qui. Et voluptatum impedit blanditiis corporis libero dolorum? Esse distinctio facere tenetur, possimus veniam vel aut tempore animi laborum. Ullam veritatis tempora, doloremque consequatur dolorem exercitationem numquam eos illum fugiat repudiandae facere at unde cum modi iusto dicta rerum officia culpa quod sed temporibus quia. Consequuntur quibusdam id ipsam pariatur, dolorum, minima exercitationem enim eum tempora, quam eius libero? Eum harum vero libero!'
-        ]
-    ];
-
-    $new_post = [];
-    foreach($blog_posts as $post){
-        if ($post["slug"] === $slug) {
-            $new_post = $post;
-        }
-    }
-
     return view('post', [
         "title" => "Post",
-        "post" => $new_post,
+        "post" => Post::find($slug),
     ]);
 });
